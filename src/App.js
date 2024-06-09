e07602d62b484a01552cf8765fe3a9463bd9bf6a
ajinkya8010 <ajinkya8975@gmail.comcom>
2024-06-09 23:11:25 +0530

    errors-solved

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <BrowserRouter basename='/ECommerce-Frontend-React'>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Shop/>}/>
+        <Route exact path='/ECommerce-Frontend-React' element={<Shop/>}/>
         <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
         <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
         <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
