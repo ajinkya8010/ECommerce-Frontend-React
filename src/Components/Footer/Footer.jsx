a5c88e20bf41098cface05f7df92b17559484dac
ajinkya8010 <ajinkya8975@gmail.com>
2025-06-10 12:49:34 +0530

    navbar-styling enhanced

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,16 +1,11 @@
-import React from 'react'
+
 import './Footer.css'
-import footer_logo from '../Assets/logo_bigg.png'
 import instagram_icon from '../Assets/instagram_icon.png'
 import pintester_icon from '../Assets/pintester_icon.png'
 import whatsapp_icon from '../Assets/whatsapp_icon.png'
 const Footer = () => {
   return (
     <div className='footer'>
-        <div className="footer-logo">
-            <img src={footer_logo} alt="" />
-            <p>BUY5</p>
-        </div>
         <ul className="footer-links">
             <li>Company</li>
             <li>Products</li>
